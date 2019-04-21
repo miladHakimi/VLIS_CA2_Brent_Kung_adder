76a672a5b9b3e32b21e0b9dc963be10b31d4dd78
miladHakimi <miladhakimi33@gmail.com>
2019-04-21 17:52:38 +0430

    sixth layer added.

--- Brent_Kung.v
+++ Brent_Kung.v
@@ -38,7 +38,7 @@ module brent_kung (
 	gray_cell gc_l2(black_cell_first_layer_g[0], black_cell_first_layer_p[0], first_layer_gray_cell_out, second_layer_gray_cell_out);
 	buf(first_layer_gray_cell_out, second_layer_buffer_g[0]);
 	generate
-	for (index=1; index < 3; index=index+1)  
+	for (index=1; index < 4; index=index+1)  
 	  begin: second_level_black_cells  
 	    black_cell black_level_2(	
 	    		black_cell_first_layer_g[2*index], 
@@ -86,5 +86,32 @@ module brent_kung (
 	wire fifth_layer_gray_cell_out;
 	gray_cell gc_l5(third_layer_buffer_g[1], third_layer_buffer_p[1], forth_layer_buffer_out, fifth_layer_gray_cell_out);
 
+	// layer 6
+	wire [2:0] sixth_layer_gray_cell_out, sixth_layer_buffer_out;
+	gray_cell gc_l6_0(
+		second_layer_buffer_g[1],
+		second_layer_buffer_g[1],
+		third_layer_buffer_g[0],
+
+		sixth_layer_gray_cell_out[0]
+	);
+	gray_cell gc_l6_1(
+		second_layer_buffer_g[2],
+		second_layer_buffer_p[2],
+		forth_layer_buffer_out,
+
+		sixth_layer_gray_cell_out[1]
+	);
+
+	gray_cell gc_l6_2(
+		second_layer_buffer_g[3],
+		second_layer_buffer_p[3],
+		fifth_layer_gray_cell_out,
+
+		sixth_layer_gray_cell_out[2]
+	);
+
+	buf(fifth_layer_gray_cell_out, sixth_layer_buffer_out[0]);
+	buf(forth_layer_gray_cell_out, sixth_layer_buffer_out[1]);
 
 endmodule
