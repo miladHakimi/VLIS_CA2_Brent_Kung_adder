60d6e23a19c422a31d00e8890943d37020284306
miladHakimi <miladhakimi33@gmail.com>
2019-04-21 09:33:56 +0430

    balck_cell added.

--- black_cell.v
+++ black_cell.v
@@ -1,8 +1,13 @@
 module black_cell (
-	input clk,    // Clock
-	input clk_en, // Clock Enable
-	input rst_n,  // Asynchronous reset active low
-	
+	input G_i_to_k,  
+	input P_i_to_k, 
+	input G_k_to_j,
+	input P_k_to_j,
+
+	output G_i_to_j,
+	output P_i_to_j
 );
+	assign G_i_to_j = (P_i_to_k & G_k_to_j) | G_i_to_k;
+	assign P_i_to_j = P_i_to_k & P_k_to_j;
 
 endmodule
